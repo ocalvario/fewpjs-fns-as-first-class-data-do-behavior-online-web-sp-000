a881b087625342d3c2c8480e3c294a8857ed7cc2
Oswald Calvario <oswald.calvario@gmail.com>
2020-07-10 22:59:29 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,7 +1,5 @@
 /* Given Code, don't edit */
 
-document.querySelector("button").addEventListener("click", handleClick);
-
 function handleClick(e) {
   const timeString = document.getElementById('time').value
   displayMessage(greet(timeString))
@@ -10,4 +8,24 @@ function handleClick(e) {
 /* End Given Code, don't edit above here...*/
 
 /* Write your implementation of greet() */
+function greet(time) {
+	let timeArray = time.split(":")
+	let hour = parseFloat(timeArray[0])
+	if (hour === NaN) {
+		return "Incorrect format"
+	}
+	else if (hour < 12) {
+		return "Good Morning"
+	} 
+	else if (hour > 17) {
+		return "Good Evening"
+	}
+	else if (hour >= 12 && hour <= 17) {
+		return "Good Afternoon"
+	}
+}
 /* Write your implementation of displayMessage() */
+function displayMessage(string) {
+	document.querySelector("#greeting").innerText = string
+	return
+}
